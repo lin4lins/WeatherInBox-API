5c2c944ccaeee66454d8fdfa92a75014fce4eeae
Elina Shramko <elina.vl.shramko@gmail.com>
2023-04-14 16:12:55 +0300

    Add TokenObtainPairView class to insert user id into rest_framework_simplejwt.views.TokenObtainPairView. response

--- weather_reminder/settings.py
+++ weather_reminder/settings.py
@@ -42,8 +42,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=5),
-    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=5),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
 }
 
 # Application definition
