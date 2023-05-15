893c46b06c5041ef74b92e3b52c8b5a2d35ad825
Elina Shramko <elina.vl.shramko@gmail.com>
2023-05-15 20:28:02 +0300

    Create a beat_schedule to execute sending weather data for actual subscriptions every hour

--- api/tasks.py
+++ api/tasks.py
@@ -1,30 +1,72 @@
+import json
 from datetime import datetime
 
+import requests
 from celery import shared_task
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
-from retry import retry
 
-from api.models import Subscription, Weather
+from api.models import City, Subscription, Weather
+from api.serializers import WeatherSerializer
 
 
-@retry(tries=2, delay=3)
+@shared_task
+def send_weather_for_actual_subscriptions():
+    current_hour = datetime.now().hour
+    active_subs_at_current_hour = [subscription for subscription in Subscription.objects.filter(is_active=True)
+                                   if current_hour % (24 // subscription.times_per_day) == 0]
+    create_actual_weather_records(active_subs_at_current_hour)
+    send_weather_notifications(active_subs_at_current_hour)
+
+    return 'Weather data have been send successfully.'
+
+
+def create_actual_weather_records(subs: list):
+    cities_with_active_subs = City.objects.filter(subscriptions__in=subs).distinct()
+    for city in cities_with_active_subs:
+        Weather.objects.create_and_populate_from_api(city=city)
+
+
+def send_weather_notifications(subs: list):
+    for sub in subs:
+        if sub.user.receive_emails:
+            send_weather_via_email.delay(sub.id)
+
+        if sub.user.webhook_url:
+            send_weather_to_webhook_url.delay(sub.id)
+
+
 @shared_task
 def send_weather_via_email(sub_id: int):
-    subscription = Subscription.objects.get(id=sub_id)
-    weather_obj, created = Weather.objects.get_or_create(city=subscription.city)
-    recipient_email_address = subscription.user.email
+    sub = Subscription.objects.get(id=sub_id)
+    weather = Weather.objects.filter(city=sub.city).order_by('-created_at').first()
+    recipient_email_address = sub.user.email
     html_message = render_to_string(
-        "weather_email.html",
-        {"weather": weather_obj},
+        'weather_email.html',
+        {'weather': weather},
     )
 
     send_mail(
-        subject=f"Weather Update",
+        subject=f'Weather Update',
         message='',
         from_email='weatherinbox.noreply@gmail.com',
         recipient_list=[recipient_email_address],
         html_message=html_message,
     )
 
-    return f"{subscription.city} data sent at {datetime.now()}"
+    return f'Email for {sub.city.name} to {sub.user.email} sent at {datetime.now()}'
+
+
+@shared_task
+def send_weather_to_webhook_url(sub_id: int):
+    sub = Subscription.objects.get(id=sub_id)
+    weather = Weather.objects.filter(city=sub.city).order_by('-created_at').first()
+    weather_serializer = WeatherSerializer(weather)
+    weather_data_json = json.dumps(weather_serializer.data)
+    url = sub.user.webhook_url
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url, data=weather_data_json, headers=headers)
+    if response.status_code != 200:
+        return response.content
+
+    return f'Request for {sub.city.name} to {sub.user.webhook_url} sent at {datetime.now()}'
--- weather_reminder/celery.py
+++ weather_reminder/celery.py
@@ -1,22 +1,10 @@
 """
 https://docs.celeryq.dev/en/stable/django/first-steps-with-django.html
 """
-import json
-import math
 import os
-import time
-from datetime import datetime
-
-import django
-from django.utils import timezone
-
-django.setup()
 
 from celery import Celery
-from celery.utils.log import get_task_logger
-from django_celery_beat.models import IntervalSchedule, PeriodicTask
-
-from api.models import Subscription
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_reminder.settings')
 
@@ -24,50 +12,10 @@ app = Celery('api')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
-logger = get_task_logger(__name__)
 
-
-def schedule_new_subscription(sub_id):
-    sub = Subscription.objects.get(id=sub_id)
-    task_interval_seconds = calculate_task_interval_seconds(sub.times_per_day)
-    interval, is_interval_created = IntervalSchedule.objects.get_or_create(every=task_interval_seconds,
-                                                                           period=IntervalSchedule.SECONDS)
-    PeriodicTask.objects.create(
-        name=f'{sub.id}',
-        task='api.tasks.send_weather_via_email',
-        interval=interval,
-        args=json.dumps([sub.id]),
-        start_time=calculate_schedule_datetime(sub.times_per_day)
-    )
-
-    logger.info(f'Periodic task for {sub.id=} have been scheduled successfully')
-
-
-def calculate_task_interval_seconds(task_frequency: int) -> float:
-    seconds_in_day = 24 * 60 * 60
-    task_interval_seconds = seconds_in_day / task_frequency
-    return task_interval_seconds
-
-
-def calculate_schedule_datetime(task_frequency: int) -> datetime:
-    """
-    Calculates the next scheduled datetime for a task given its frequency in a day.
-
-    Args:
-        task_frequency: Number of times the task is scheduled to run per day.
-
-    Returns:
-        datetime: Next scheduled datetime for the task.
-    """
-    current_timestamp = time.time()
-    today_midnight = datetime.combine(datetime.today(), datetime.min.time())
-    today_midnight_timestamp = today_midnight.timestamp()
-
-    task_interval_seconds = calculate_task_interval_seconds(task_frequency)
-    elapsed_seconds_since_midnight = current_timestamp - today_midnight_timestamp
-    next_task_index = math.ceil(elapsed_seconds_since_midnight / task_interval_seconds)
-    next_task_time_seconds = task_interval_seconds * next_task_index
-    next_schedule_timestamp = next_task_time_seconds + today_midnight_timestamp
-    naive_datetime = datetime.fromtimestamp(next_schedule_timestamp)
-    aware_datetime = timezone.make_aware(naive_datetime, timezone.get_default_timezone())
-    return aware_datetime
+app.conf.beat_schedule = {
+    'send-weather-notification': {
+        'task': 'api.tasks.send_weather_for_actual_subscriptions',
+        'schedule': crontab(hour='*'),
+    },
+}
